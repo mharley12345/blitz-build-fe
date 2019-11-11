bd8d8bc9e32aadc50c9a35218c983995f86e65b9
Connor Holly <connorholly11@gmail.com>
2019-11-11 10:24:33 -0700

    login done, projects WIP

--- blitz-build/src/components/auth/Login.jsx
+++ blitz-build/src/components/auth/Login.jsx
@@ -1,21 +1,65 @@
-import React from "react";
-import { Link } from "react-router-dom"
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-function Login() {
+const Login = props => {
+  const [form, setForm] = useState({ email: "", password: "" });
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      props.history.push("/dashboard");
+    } else {
+      console.log("no token");
+    }
+  }, [props.history]);
+
+  const changeHandler = e => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const submitForm = e => {
+    e.preventDefault();
+
+    axios
+      .post("https://api-blitz-build-dev.herokuapp.com/api/login", form)
+      .then(res => {
+        console.log(res);
+
+        localStorage.setItem("uid", res.data.userObj.user.uid);
+        localStorage.setItem("token", res.data.token);
+
+        props.history.push("/dashboard");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   return (
     <div>
       <h1>Login</h1>
-      <p>Already have an account? <Link to="/signup"> Sign up</Link></p>
-      <form>
-        <label>Email</label>
-        <input placeholder="john.doe@gmail.com" />
-        <label>Password</label>
-        <input placeholder="••••••••" />
-        <button>Log in</button>
+      <p>
+        Don't have an account? <Link to="/signup"> Sign up</Link>
+      </p>
+      <form onSubmit={submitForm}>
+        <input
+          name="email"
+          placeholder="email"
+          onChange={changeHandler}
+          value={form.email}
+        />
+        <input
+          name="password"
+          placeholder="password"
+          onChange={changeHandler}
+          value={form.password}
+        />
+        <button type="submit">Log in</button>
       </form>
     </div>
   );
-}
+};
 
 export default Login;
-
