325a855c907284aca0b67ffda57dbc6d509a53eb
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2019-11-12 13:00:44 -0600

    created a task form component

---
+++ blitz-build/src/components/tasks/TaskForm.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function TaskForm({ closeModal }) {
+    return (
+        <form>
+            <button onClick= { closeModal }>x</button>
+
+            <label>Task Name</label>
+            <input/>
+
+            <label>Task Decription</label>
+            <input/>
+
+            <label>Due Date</label>
+            <input/>
+
+            <label>Assign Project</label>
+            <input/>
+            
+            <button>cancel</button>
+            <button>add task</button>
+        </form>
+    )
+}
