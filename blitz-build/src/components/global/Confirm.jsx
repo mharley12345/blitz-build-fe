5581cb649f17471e0697c1e303e38edccdfa2afc
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2019-11-14 15:31:59 -0600

    added reusability to confirm modal

--- blitz-build/src/components/global/Confirm.jsx
+++ blitz-build/src/components/global/Confirm.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 
-export default function Confirm({ closeModal, text }) {
+export default function Confirm({ closeModal, text, deleteFunction, deleteItem }) {
     return (
         <div>
             <button onClick={ closeModal }>X</button>
             <h1>{text}</h1>
             <p>Are you sure you want to delete?</p>
             <button onClick={ closeModal }>Cancel</button>
-            <button>Delete</button>
-
+            <button onClick={ () => {
+                deleteFunction(deleteItem)
+                closeModal()
+            } }>Delete</button>
         </div>
     )
 }
