7df3ec9cd6f3aa097e416d14fc54c6c0e7062a06
John Rojcewicz <rojcewiczj@gmail.com>
2019-11-18 11:38:54 -0600

    added logo and avatar to nav bar

--- blitz-build/src/layouts/Nav.js
+++ blitz-build/src/layouts/Nav.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import styled from 'styled-components'
+import Logo from '../styles/Logo/Logo.png'
+import Avatar from '../styles/Avatar/Avatar.png'
 
 function Nav() {
     return (
         <Container>
-            <p>This is nav</p>
+            <LogoContainer>
+           <img  src={Logo} alt="Blitz-Build-Logo"/>
+           </LogoContainer>
+           <UserProfile>
+               <img src={Avatar} alt="Blitz-Build-Avatar"/>
+           </UserProfile>
         </Container>
     )
 }
@@ -15,4 +22,15 @@ const Container = styled.div`
   min-width: 296px;
   height: 100vh;
   background: #3f3a36;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
+const LogoContainer = styled.div`
+  margin: 29.25px 108px 35px 74.8px;
+`
+const UserProfile = styled.div`
+
+width: 155px;
+height: 48px;
+`
