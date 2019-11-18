873efa13f1b2ac0cb8ab7978daf9d72c3bfef24f
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2019-11-18 09:22:18 -0600

    refactured for database fields

--- blitz-build/src/components/tasks/Task.jsx
+++ blitz-build/src/components/tasks/Task.jsx
@@ -10,10 +10,10 @@ export default function Task({ task }) {
   return (
     <>
       <div>
-        <h1 style={{ margin: 0 }}> {task.name}</h1>
-        <p style={{ margin: 0 }}>{task.description}</p>
-        <TaskStatus status={"today"} />
+        <h1 style={{ margin: 0 }}> {task.task_name}</h1>
+        <p style={{ margin: 0 }}>{task.task_description}</p>
       </div>
+        <TaskStatus status={"today"} />
       <EditTask task={ task }/>
 			<DeleteTask task={ task }/>
     </>
