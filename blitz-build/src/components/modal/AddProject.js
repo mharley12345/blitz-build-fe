5e6ca92d69a481be56b61fcbb1066d7f5e61be44
John Rojcewicz <rojcewiczj@gmail.com>
2019-11-20 13:08:26 -0600

    mostly finsihed with styling for add project modal

--- blitz-build/src/components/modal/AddProject.js
+++ blitz-build/src/components/modal/AddProject.js
@@ -8,45 +8,74 @@ import axios from "axios";
 import styled from "styled-components";
 import zipcodes from "zipcodes";
 import OpenContext from "../../contexts/projects/OpenContext";
+import { Hidden } from "@material-ui/core";
 
+const ModalContainer =styled.div`
+overflow-y: visible;
+width: '750px',
+height: '1000px',
+background: '#FFFFFF',
+border-radius: '3px',
+display:'flex',
+flex-direction: 'column',
+justify-Content: 'center',
+align-items: 'center',
+`
 
+const DialogStyle = {
+// overflowY: 'visible',
+// margin: '0 auto',
+// width: '810px',
+// height: '1000px',
+// background: '#FFFFFF',
+// borderRadius: '3px',
+// display:'flex',
+// flexDirection: 'column',
+// justifyContent: 'center',
+// alignItems: 'center',
 
-const ModalContainer = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-width: 800px;
-height: 1078px;
-background: #FFFFFF;
-border: 1px solid #8A827D;
-border-radius: 3px;
-`;
+}
 
+const DialogActionsStyle = {
+  height: '25px',
+  width: '550px'
+}
+const CancelButtonStyle = {
+ marginTop: '10px',
+  fontSize: '30px',
+  color: '#000000'
+}
 const ModalTitle = styled.div`
+   
   display: flex;
   flex-direction: column;
-  width: 656px;
-  height: 400px;
+  width: 400px;
+  height: 300px;
   justify-content: center;
+  align-Items: center;
+  margin: 0 auto;
+
 `;
 
 const TitleText = styled.div`
-
+margin-bottom: 15px;
 font-style: normal;
 font-weight: 500;
 font-size: 36px;
 line-height: 42px;
 color: #3B3B3B;
+ margin-top: -40px;
 `;
 const ImgDrop = styled.div`
 display: flex;
 justify-content: center;
 align-content: center;
-width: 656px;
-height: 295px;
+width: 350px;
+height: 200px;
 background: #FAFAFA;
 border: 1px solid #8A827D;
 border-radius: 3px;
+margin: '0 auto';
 `;
 const DropTextContainer = styled.div`
 display: flex;
@@ -74,32 +103,39 @@ color: #3B3B3B;
 `
 
 const formStyle = {
+  margin: '0 auto',
 display: 'flex',
 justifyContent: 'center',
-width: '656px',
+width: '500px',
 height: '500px',
 flexDirection: 'column',
-alignItems: 'center'
+alignItems: 'center',
+overflow: 'hidden',
+marginBottom: '20px'
 }
 const inputStyle = {
 display: 'flex',
-width: '656px',
+width: '390px',
 height: '72px',
 margin: '15px 0px',
 flexDirection: 'column',
 
 }
 const buttonStyle = {
+  border: 'none',
+  textDecoration: 'none',
   width: '163px',
-  height: '48px',
+  height: '60px',
   fontSize: '16px',
   color: '#FFFFFF',
   lineHeight: '19px',
   backgroundColor: '#DA552F',
-  borderRadius: '3px'
+  borderRadius: '3px',
+  
 }
 
-
+const DialogContentStyle = {
+}
 
 //START OF FUNCTIONAL COMPONENT
 
@@ -164,9 +200,15 @@ const AddProject = props => {
         open={open}
         onClose={handleClose}
         aria-labelledby="form-dialog-title"
-        
+        style={DialogStyle}
       >
-      <ModalContainer>
+        <ModalContainer>
+        <DialogActions style = {DialogActionsStyle}>
+          <Button onClick={handleClose} style={CancelButtonStyle}>
+            X
+          </Button>
+        </DialogActions>
+
         <ModalTitle>
           
           {/* <DialogTitle id="form-dialog-title">Subscribe</DialogTitle> */}
@@ -178,7 +220,7 @@ const AddProject = props => {
          </DropTextContainer>
           </ImgDrop>
         </ModalTitle>
-        <DialogContent>
+        <DialogContent style= {DialogContentStyle}>
           
           <form onSubmit={submitForm} style={formStyle}>
             {/* <TopContainer> */}
@@ -252,15 +294,8 @@ const AddProject = props => {
           </form>
          
         </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Cancel
-          </Button>
-          {/* <Button onSubmit={submitForm} color="primary">
-            Add Project
-          </Button> */}
-        </DialogActions>
-       </ModalContainer>
+        </ModalContainer>
+       
       </Dialog>
       
   );
