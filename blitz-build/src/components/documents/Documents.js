6ce3608fc9d85eba4c8f0aa45877ae8dac4c94cd
Mike Harley <mharley12345@gmail.com>
2019-12-21 16:52:19 -0800

    :wrench: styling changes

--- blitz-build/src/components/documents/Documents.js
+++ blitz-build/src/components/documents/Documents.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import DocumentsContext from '../../contexts/documents/DocumentsContext'
 import {Table} from 'react-bootstrap'
 
-import {axiosWithAuth} from '../../utils/auth/axiosWithAuth'
+
 import './table.css'
 const user_id = localStorage.getItem("user_id")
 const file_name = localStorage.getItem("file_name")
@@ -11,13 +11,13 @@ const  DocumentCard = (props) => {
   const {document} =
   useContext(DocumentsContext)
 console.log(user_id,file_name,"TESTING DOCS")
- const handleDelete = (ev) =>{
+//  const handleDelete = (ev) =>{
 
 
-     axiosWithAuth().delete(`/docs/url/${file_name}`,user_id)
+//      axiosWithAuth().delete(`/docs/url/${file_name}`,user_id)
    
      
- }
+//  }
 
 
   return (
@@ -43,14 +43,14 @@ console.log(user_id,file_name,"TESTING DOCS")
             
             
              <tbody  >
-               <tr  onSubmit={handleDelete()}>
+               <tr>
                  <td >{documents.file_name}</td>
                  <td className="hide" value="false">{documents.project_name}</td>
                  <td className="hide" value="false">{documents.createdAt}</td>
                  <td> 
                  <a href ={documents.doc_url} 
                   rel="noopener noreferrer" target="_blank">
-                  View</a> ><button type="submit">Delete</button>
+                  View</a> >
                   </td>
              
                </tr>
