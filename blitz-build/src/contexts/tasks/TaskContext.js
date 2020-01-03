fb44e623215ba7115a73d79e439f92258601e951
John Rojcewicz <rojcewiczj@gmail.com>
2020-01-03 16:13:44 -0600

    working on testing

--- blitz-build/src/contexts/tasks/TaskContext.js
+++ blitz-build/src/contexts/tasks/TaskContext.js
@@ -1,7 +1,20 @@
 import { createContext, useContext } from 'react';
+import moment from "moment";
 
 const TaskContext = createContext();
+
 export const useTaskContext = () => useContext(TaskContext);
+
+export const newtask = (task) => {
+    const newtask = {
+    title: `${task.task_name} (${task.project_name})`,
+    allDay: true,
+    start: new Date(moment(task.due_date).format("L")),
+    end: new Date(moment(task.due_date).format("L"))
+}
+
+return newtask
+}
 
 
 export default TaskContext;
--- blitz-build/src/test/templates.test.js
+++ blitz-build/src/test/templates.test.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import * as TemplatesContext from '../contexts/templates/TemplateContext'
-import * as PathnameContext from '../contexts/PathnameContext'
-import Templates  from '../components/templates/templates'
-import { Name }  from '../components/templates/templates'
+import * as TemplatesContext from '../contexts/templates/TemplateContext';
+import * as PathnameContext from '../contexts/PathnameContext';
+import Templates  from '../components/templates/templates';
+import { Name }  from '../components/templates/templates';
 import styled, { css } from "styled-components";
-
+import IndividualTemplate from '../components/templates/IndividualTemplate';
+import { TitleText } from '../components/templates/IndividualTemplate';
 
 it('renders without crashing', () => {
    expect(1).toBe(1);
@@ -19,53 +20,23 @@ const setPathname =(pathname) => {
 describe('<Templates />', () => {
   test('it should mock the context', () => {
   
-    const contextValues = {  setPathname, templates: [{
+    const contextValues = {  setPathname, 
+      
+      templateTask: [{
+      id: 27296,
+      task_name: "Check the activity feed again",
+      task_description: "",
+      due_date: "2020-01-25",
+      template_id: "35"
+
+    }],
+      
+      
+      templates: [{
       template_name: "cool",
       id: 21,
   }],
-   template_tasks: [{
-      project_name: "Tacoma",
-      id: 10075,
-      user_id: "google-oauth2|117345694816613292954",
-      task_name: "Electrical Meter",
-      task_description: "",
-      due_date: "",
-      start_date: null,
-      createdAt: "2019-12-17T00:00:00.000Z",
-      isComplete: false,
-      project_id: 55,
-      template_id: null,
-      template_name: "90_day",
-    },
-      {
-        project_name: "Tacoma",
-        id: 10074,
-        user_id: "google-oauth2|117345694816613292954",
-        task_name: "Specialites(towl bars,mirror,ect)",
-        task_description: "",
-        due_date: "",
-        start_date: null,
-        createdAt: "2019-12-17T00:00:00.000Z",
-        isComplete: false,
-        project_id: 55,
-        template_id: null,
-        template_name: "90_day",
-      },
-      {
-        project_name: "Tacoma",
-        id: 10073,
-        user_id: "google-oauth2|117345694816613292954",
-        task_name: "Electrical Trim",
-        task_description: "",
-        due_date: "",
-        start_date: null,
-        createdAt: "2019-12-17T00:00:00.000Z",
-        isComplete: false,
-        project_id: 55,
-        template_id: null,
-        template_name: "90_day",
-      }
-      ] };
+}
     
     jest
       .spyOn(TemplatesContext, 'useTemplateContext')
@@ -76,10 +47,13 @@ describe('<Templates />', () => {
     // jest
     //   .spyOn(SearchTermContext, 'useSearchTermContext')
     //   .mockImplementation(() => contextValues);
-    const Wrapper = shallow(<Templates setPathname = {contextValues.setPathname} template={contextValues.templates[0]}/>);
+    const Wrapper = shallow(<Templates setPathname = {contextValues.setPathname} template={contextValues.templates[0]} />);
+
+    const TemplateTaskWrapper = shallow(<IndividualTemplate setPathname ={contextValues.setPathname} templateTask={contextValues.templateTask[0]}/>)
     const TextFind = Wrapper.find(Name).at(1);  
     expect(TextFind.text()).toBe('cool')
-
+    const TemplateTaskTextFind = TemplateTaskWrapper.find(TitleText).at(0) 
+    expect(TemplateTaskTextFind.text()).toBe('Check the activity feed again')
     
 
    
