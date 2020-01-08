bed0c428cdbebb0d1e7a917b532440df371a1aa4
Mike Harley <mharley12345@gmail.com>
2020-01-08 09:42:28 -0800

    :wrench: updates comments

--- blitz-build/src/components/documents/SingleProjectDocCard.js
+++ blitz-build/src/components/documents/SingleProjectDocCard.js
@@ -4,7 +4,16 @@ import DocumentsContext from '../../contexts/documents/DocumentsContext'
 import styled from "styled-components";
 import Global from '../../styles/Global';
 
-
+/** SingleProjectDocCard
+ *  The SingleProjectDocCard componet is almost the same component as the
+ *   Documents component except it only displays documents for one specific 
+ *   project.
+ *
+ */
+/** TODO  
+ *  The component currently opens the document in a seperate window
+ *  we may want to have it open in the ViewDocument component
+ */
 const SingleProjectDocCard = (props) =>{
     const {documents} = useContext(DocumentsContext)
  
